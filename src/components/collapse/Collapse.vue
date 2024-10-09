598f525eb3e54bd07ced5f10a8dcaaec8e0f7691
kkippy <2731909292@qq.com>
2024-10-09 17:10:09 +0800

    feat:初步搭建collapse组件

---
+++ src/components/collapse/Collapse.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="k-collapse">
+    <slot></slot>
+
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+export default defineComponent({
+  name: 'Collapse'
+})
+</script>
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/collapse/CollapseItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="k-collapseItem">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+<script lang="ts">
+import {defineComponent} from 'vue'
+export default defineComponent({
+  name: 'CollapseItem'
+})
+</script>
+<style scoped>
+
+</style>
